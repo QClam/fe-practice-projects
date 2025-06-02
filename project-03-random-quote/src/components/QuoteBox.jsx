025fbc86e0c8e093d89ba708b1484cc10ae049c3
Xuan Quy <hoangxuanquy5@gmail.com>
2025-06-02 10:57:35 +0700

    Completed Project-03-random-quote

--- project-03-random-quote/src/components/QuoteBox.jsx
+++ project-03-random-quote/src/components/QuoteBox.jsx
@@ -1,65 +1,66 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 
 function QuoteBox() {
-  const quotes = [
-    "Cuộc sống là một món quà.",
-    "Hãy sống như thể bạn sẽ chết vào ngày mai.",
-    "Không có gì là không thể nếu bạn đủ kiên trì.",
-    "Thành công không phải là đích đến, mà là hành trình.",
-    "Hạnh phúc là biết đủ.",
-  ];
-
   const colors = [
-    "bg-violet-400",
-    "bg-red-400",
-    "bg-pink-400",
-    "bg-amber-400",
-    "bg-yellow-400",
-    "bg-rose-400",
-    "bg-orange-400",
+    "bg-violet-500",
+    "bg-red-500",
+    "bg-pink-500",
+    "bg-amber-500",
+    "bg-yellow-500",
+    "bg-rose-500",
+    "bg-orange-500",
   ];
 
-  const [currentQuote, setCurrentQuote] = useState("Cuộc sống là một món quà.");
-  const [currentBgColor, setCurrentBgColor] = useState("bg-violet-400");
+  const [currentQuote, setCurrentQuote] = useState({ content: "", author: "" });
+  const [currentBgColor, setCurrentBgColor] = useState("bg-violet-500");
 
-  const handleNewQuote = () => {
-    let randomIndex;
-    do {
-      randomIndex = Math.floor(Math.random() * quotes.length);
-    } while (quotes[randomIndex] === currentQuote);
-    setCurrentQuote(quotes[randomIndex]);
+  const fetchQuotes = async () => {
+    try {
+      const response = await fetch("https://api.quotable.io/random");
+      const data = await response.json();
 
-    //Math.florr(): làm tròn xuống; Math.random(): lấy ngẫu nhiên index trong mảng
-    let randomBgColor;
-    do {
-      randomBgColor = Math.floor(Math.random() * colors.length);
-    } while (colors[randomBgColor] === currentBgColor);
-    setCurrentBgColor(colors[randomBgColor]);
+      //Math.florr(): làm tròn xuống; Math.random(): lấy ngẫu nhiên index trong mảng
+      let randomBgColor;
+      do {
+        randomBgColor = Math.floor(Math.random() * colors.length);
+      } while (colors[randomBgColor === currentBgColor]);
+
+      setCurrentBgColor(colors[randomBgColor]);
+      setCurrentQuote({ content: data.content, author: data.author });
+
+    } catch (error) {
+      alert("Có lỗi xảy ra trong quá trình lấy Quote", error);
+    }
   };
+
+  useEffect(() => {
+    fetchQuotes()
+  },[])
 
   return (
     <div
       className={`rounded-2xl p-4 w-[550px] text-white transition-all duration-300 ${currentBgColor}`}
     >
       <AnimatePresence mode="wait">
-        <motion.p
-          key={currentQuote}
+        <motion.div
+          key={currentQuote.content}
           initial={{ opacity: 0, y: -20 }}
           animate={{ opacity: 1, y: 0 }}
           exit={{ opacity: 0, y: 20 }}
           transition={{ duration: 0.5 }}
           className="text-lg italic"
         >
-          {currentQuote}
-        </motion.p>
+          <p className="italic text-xl mb-2">"{currentQuote.content}"</p>
+          <p className="text-sm text-right">- {currentQuote.author}</p>
+        </motion.div>
       </AnimatePresence>
 
       <button
         className="rounded p-2 bg-white text-black font-bold my-4"
-        onClick={handleNewQuote}
+        onClick={fetchQuotes}
       >
-        Quote mới
+        New Quote
       </button>
     </div>
   );
